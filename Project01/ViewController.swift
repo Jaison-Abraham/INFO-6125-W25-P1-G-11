94abc4b50c46286ea48819ca2ec217db16ad8b6a
Stalinjoy2001 <stalinjoy005@gmail.com>
2025-02-19 00:36:01 -0500

    added UI

--- Project01/ViewController.swift
+++ Project01/ViewController.swift
@@ -8,10 +8,100 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var row1Col1: UILabel!
+    @IBOutlet weak var row1Col2: UILabel!
+    @IBOutlet weak var row1Col3: UILabel!
+    @IBOutlet weak var row1Col4: UILabel!
+    @IBOutlet weak var row1Col5: UILabel!
+    
+    @IBOutlet weak var row2Col1: UILabel!
+    @IBOutlet weak var row2Col2: UILabel!
+    @IBOutlet weak var row2Col3: UILabel!
+    @IBOutlet weak var row2Col4: UILabel!
+    @IBOutlet weak var row2Col5: UILabel!
+    
+    @IBOutlet weak var row3Col1: UILabel!
+    @IBOutlet weak var row3Col2: UILabel!
+    @IBOutlet weak var row3Col3: UILabel!
+    @IBOutlet weak var row3Col4: UILabel!
+    @IBOutlet weak var row3Col5: UILabel!
+    
+    @IBOutlet weak var row4Col1: UILabel!
+    @IBOutlet weak var row4Col2: UILabel!
+    @IBOutlet weak var row4Col3: UILabel!
+    @IBOutlet weak var row4Col4: UILabel!
+    @IBOutlet weak var row4Col5: UILabel!
+    
+    @IBOutlet weak var row5Col1: UILabel!
+    @IBOutlet weak var row5Col2: UILabel!
+    @IBOutlet weak var row5Col3: UILabel!
+    @IBOutlet weak var row5Col4: UILabel!
+    @IBOutlet weak var row5Col5: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        row1Col1.layer.masksToBounds = true
+        row1Col1.layer.cornerRadius = 8
+        
+        row1Col1.text = "H"
+//        row1Col2.layer.masksToBounds = true
+//        row1Col2.layer.cornerRadius = 8
+//        row1Col3.layer.masksToBounds = true
+//        row1Col3.layer.cornerRadius = 8
+//        row1Col4.layer.masksToBounds = true
+//        row1Col4.layer.cornerRadius = 8
+//        row1Col5.layer.masksToBounds = true
+//        row1Col5.layer.cornerRadius = 8
+//        
+//        row2Col1.layer.masksToBounds = true
+//        row2Col1.layer.cornerRadius = 8
+//        row2Col2.layer.masksToBounds = true
+//        row2Col2.layer.cornerRadius = 8
+//        row2Col3.layer.masksToBounds = true
+//        row2Col3.layer.cornerRadius = 8
+//        row2Col4.layer.masksToBounds = true
+//        row2Col4.layer.cornerRadius = 8
+//        row2Col5.layer.masksToBounds = true
+//        row2Col5.layer.cornerRadius = 8
+//        
+//        
+//        row3Col1.layer.masksToBounds = true
+//        row3Col1.layer.cornerRadius = 8
+//        row3Col2.layer.masksToBounds = true
+//        row3Col2.layer.cornerRadius = 8
+//        row3Col3.layer.masksToBounds = true
+//        row3Col3.layer.cornerRadius = 8
+//        row3Col4.layer.masksToBounds = true
+//        row3Col4.layer.cornerRadius = 8
+//        row3Col5.layer.masksToBounds = true
+//        row3Col5.layer.cornerRadius = 8
+//        
+//        row4Col1.layer.masksToBounds = true
+//        row4Col1.layer.cornerRadius = 8
+//        row4Col2.layer.masksToBounds = true
+//        row4Col2.layer.cornerRadius = 8
+//        row4Col3.layer.masksToBounds = true
+//        row4Col3.layer.cornerRadius = 8
+//        row4Col4.layer.masksToBounds = true
+//        row4Col4.layer.cornerRadius = 8
+//        row4Col5.layer.masksToBounds = true
+//        row4Col5.layer.cornerRadius = 8
+//        
+//        row5Col1.layer.masksToBounds = true
+//        row5Col1.layer.cornerRadius = 8
+//        row5Col2.layer.masksToBounds = true
+//        row5Col2.layer.cornerRadius = 8
+//        row5Col3.layer.masksToBounds = true
+//        row5Col3.layer.cornerRadius = 8
+//        row5Col4.layer.masksToBounds = true
+//        row5Col4.layer.cornerRadius = 8
+//        row5Col5.layer.masksToBounds = true
+//        row5Col5.layer.cornerRadius = 8
+        
+        
     }
 
 
